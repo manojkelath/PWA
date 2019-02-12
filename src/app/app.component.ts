510fcfc46f6db3cbc748b75d2bd325367ccb7096
Manoj Kelath <supercodepanda@gmail.com>
2019-02-12 13:35:24 -0500

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,36 +1,31 @@
-import {Component, OnInit} from '@angular/core';
-import {SwPush, SwUpdate} from "@angular/service-worker";
-
+import { Component } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent  implements OnInit {
+export class AppComponent {
+  title = 'app';
 
+  update: boolean = false;
+  joke: any;
 
-    constructor(private swUpdate: SwUpdate) {
+  constructor(updates: SwUpdate, private data: DataService) {
+    updates.available.subscribe(event => {
 
-    }
+      //this.update = true;
+      updates.activateUpdate().then(() => document.location.reload());
 
-    ngOnInit() {
+    })
+  }
 
-        if (this.swUpdate.isEnabled) {
-
-            this.swUpdate.available.subscribe(() => {
-
-                if (confirm("New version available. Load New Version?")) {
-                    window.location.reload();
-                }
-
-            });
-
-        }
-
-
-
-    }
+  ngOnInit() {
+    this.data.gimmeJokes().subscribe(res => {
+      this.joke = res;
+    })
+  }
 
 }
-
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,51 +1,23 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-import {HttpClientModule} from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import {AppComponent} from './app.component';
-import {LessonsComponent} from './lessons/lessons.component';
+import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
-import {LessonsService} from "./services/lessons.service";
-import {ReactiveFormsModule} from "@angular/forms";
-
-import {environment} from '../environments/environment';
-import {ServiceWorkerModule} from '@angular/service-worker';
-
-import 'rxjs/add/operator/switchMap';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/shareReplay';
-import 'rxjs/add/operator/do';
-import 'rxjs/add/operator/filter';
-import 'rxjs/add/operator/catch';
-import 'rxjs/add/operator/first';
-import 'rxjs/add/observable/of';
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {AppRoutingModule} from "./app-routing.module";
-import {NewsletterService} from "./services/newsletter.service";
-
-
-
+import { HttpClientModule } from '@angular/common/http';
+import { DataService } from './data.service';
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        LessonsComponent
-    ],
-    imports: [
-        BrowserModule.withServerTransition({ appId: 'serverApp' }),
-        HttpClientModule,
-        BrowserAnimationsModule,
-        AppRoutingModule,
-        ReactiveFormsModule,
-        ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production }),
-
-    ],
-    providers: [
-        LessonsService,
-        NewsletterService
-    ],
-    bootstrap: [AppComponent]
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
+  ],
+  providers: [DataService],
+  bootstrap: [AppComponent]
 })
-export class AppModule {
-
-}
+export class AppModule { }
